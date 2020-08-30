6e9581bc5c584eb38f668a0a42bbd6885a741ac1
Sourlun <1141837289@qq.com>
2020-08-30 21:34:22 +0800

    添加说明注解

--- user-service-provider/src/main/java/com/sour/mall/BootUserServiceProviderApplication.java
+++ user-service-provider/src/main/java/com/sour/mall/BootUserServiceProviderApplication.java
@@ -3,6 +3,7 @@ package com.sour.mall;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  *
@@ -13,13 +14,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  *  1,  导入dubbo.starter依赖
  *      https://github.com/apache/dubbo-spring-boot-project
- *  2,
  *
+ *  springboot与dubbo整合的三种方式
+ *      一,  导入dubbo-starter, 在application.properties配置属性, 使用@Service[暴露服务],
+ *              使用@Reference[引用服务]
+ *      二,  保留dubbo xml的配置文件
+ *          @ImportResource(locations = "classpath:provide.xml")
+ *      三,  使用注解API的方式
+ *          重写配置类
  *
  *
  *
  **/
 @EnableDubbo//开启基于注解dubbo功能
+//@ImportResource(locations = "classpath:provide.xml") //使用配置文件的方式
 @SpringBootApplication
 public class BootUserServiceProviderApplication {
 
